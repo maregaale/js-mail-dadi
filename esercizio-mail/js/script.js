e157659af8d6ec0b5c26399d18ff8277c0ae2b35
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 17:32:12 +0100

    fix sintassi esercizio 'mail', fix stile a esercizio 'dadi'

--- esercizio-mail/js/script.js
+++ esercizio-mail/js/script.js
@@ -9,22 +9,20 @@ var messaggioPositivo = "La tua mail è nella nostra mailing-list, puoi proceder
 var messaggioNegativo = "La tua mail NON è nella nostra mailing-list, contattaci per informazioni sull'accesso!"
 
 // controllo appartenenza all'array
-// ciclo for esito negativo
-for (var i = 0; i < mailingList.length; i++) {
+var emailValida
 
-  // stampa esito
-  if ( mail != mailingList[i] ) {
-    document.getElementById('esito').innerHTML = messaggioNegativo;
-  }
-
-}
-
-// ciclo for esito positivo
 for (var i = 0; i < mailingList.length; i++) {
 
   // stampa esito
   if ( mail == mailingList[i]) {
-    document.getElementById('esito').innerHTML = messaggioPositivo;
+    console.log("email valida");
+    emailValida = mail;
   }
 
+}
+
+if (emailValida) {
+  document.getElementById('esito').innerHTML = messaggioPositivo;
+} else {
+  document.getElementById('esito').innerHTML = messaggioNegativo;
 }
