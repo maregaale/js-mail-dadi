ed508e6698f60fd54704c6f70b5fef719126f96a
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 15:59:20 +0100

    aggiunte variabili numeri random per giocatore e computer in 'dadi'

--- esercizio-dadi/js/script.js
+++ esercizio-dadi/js/script.js
@@ -1,8 +1,8 @@
 // numero random giocatore
-
+var numRandomGiocatore = Math.floor(Math.random() * 6) + 1;
 
 // numero random giocatore
-
+var numRandomComputer = Math.floor(Math.random() * 6) + 1;
 
 // verifica quale numero random è maggiore
 
