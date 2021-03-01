d9819e544324717f6d2b6f13b12cfd0e2e9f1e1e
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 15:38:03 +0100

    aggiunto controllo e stampa esito all'esercizio 'mail'

--- esercizio-mail/js/script.js
+++ esercizio-mail/js/script.js
@@ -4,7 +4,27 @@ var mail = prompt("Inserisci qui la tua mail");
 // creazione array mailing-list
 var mailingList = ["ale@hotmail.it", "pinco_pallino@gmail.com", "mario-rossi@gmail.com", "yoda@gmail.it", "forrest_gump@hotmail.it"];
 
+// messaggi
+var messaggioPositivo = "La tua mail è nella nostra mailing-list, puoi procedere con l'accesso."
+var messaggioNegativo = "La tua mail NON è nella nostra mailing-list, contattaci per informazioni sull'accesso!"
+
 // controllo appartenenza all'array
+// ciclo for esito negativo
+for (var i = 0; i < mailingList.length; i++) {
 
+  // stampa esito
+  if ( mail != mailingList[i] ) {
+    document.getElementById('esito').innerHTML = messaggioNegativo;
+  }
 
-// stampa esito
+}
+
+// ciclo for esito positivo
+for (var i = 0; i < mailingList.length; i++) {
+
+  // stampa esito
+  if ( mail == mailingList[i]) {
+    document.getElementById('esito').innerHTML = messaggioPositivo;
+  }
+
+}
