07565149d41b37cb62c94fef7dd609548202b161
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 15:00:42 +0100

    aggiunti richiesta mail e array 'mailinglist' ad esercizio 'mail'

--- esercizio-mail/js/script.js
+++ esercizio-mail/js/script.js
@@ -1,8 +1,8 @@
 // richiesta mail all'utente
-
+var mail = prompt("Inserisci qui la tua mail");
 
 // creazione array mailing-list
-
+var mailingList = ["ale@hotmail.it", "pinco_pallino@gmail.com", "mario-rossi@gmail.com", "yoda@gmail.it", "forrest_gump@hotmail.it"];
 
 // controllo appartenenza all'array
 
