a51c004c44ef3071169ec166de33c3d433e69fca
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 16:15:31 +0100

    aggiunta verifica e stampa del risultato nell'esercizio 'dadi'

--- esercizio-dadi/js/script.js
+++ esercizio-dadi/js/script.js
@@ -4,7 +4,22 @@ var numRandomGiocatore = Math.floor(Math.random() * 6) + 1;
 // numero random giocatore
 var numRandomComputer = Math.floor(Math.random() * 6) + 1;
 
+
+// messaggio risultato lancio dei Dadi
+var messaggio = "A te è uscito il numero " + numRandomGiocatore + "," + " al computer è uscito il numero " + numRandomComputer + ".";
+
+
 // verifica quale numero random è maggiore
+if (numRandomGiocatore > numRandomComputer) {
+  // stampa esito
+  document.getElementById('esito').innerHTML = messaggio + " Hai vinto!";
 
+} else if (numRandomGiocatore < numRandomComputer) {
+  // stampa esito
+  document.getElementById('esito').innerHTML = messaggio + " Hai perso.";
 
-// stampa esito
+} else {
+  // stampa esito
+  document.getElementById('esito').innerHTML = messaggio + " Siete pari!";
+  
+}
