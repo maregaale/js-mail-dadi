0f6c34da9faaa6a2023fa629830587ef9dabfafd
Alessandro Marega <ale_marega@hotmail.it>
2021-03-01 16:20:51 +0100

    fix sintassi

--- esercizio-dadi/js/script.js
+++ esercizio-dadi/js/script.js
@@ -8,18 +8,20 @@ var numRandomComputer = Math.floor(Math.random() * 6) + 1;
 // messaggio risultato lancio dei Dadi
 var messaggio = "A te è uscito il numero " + numRandomGiocatore + "," + " al computer è uscito il numero " + numRandomComputer + ".";
 
+// stampa messaggio risultato
+document.getElementById('esito').innerHTML = messaggio;
 
 // verifica quale numero random è maggiore
 if (numRandomGiocatore > numRandomComputer) {
   // stampa esito
-  document.getElementById('esito').innerHTML = messaggio + " Hai vinto!";
+  document.getElementById('result').innerHTML = " Hai vinto!";
 
 } else if (numRandomGiocatore < numRandomComputer) {
   // stampa esito
-  document.getElementById('esito').innerHTML = messaggio + " Hai perso.";
+  document.getElementById('result').innerHTML = " Hai perso.";
 
 } else {
   // stampa esito
-  document.getElementById('esito').innerHTML = messaggio + " Siete pari!";
-  
+  document.getElementById('result').innerHTML = " Siete pari!";
+
 }
